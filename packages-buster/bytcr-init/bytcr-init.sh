e30f35955ba1036c4c4eb7256591834a0f0bce5c
Gé Koerkamp <ge.koerkamp@gmail.com>
2021-02-26 16:58:11 +0100

    Alsa UCM: modified init script

--- packages-buster/bytcr-init/bytcr-init.sh
+++ packages-buster/bytcr-init/bytcr-init.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
-
+!/bin/bash
+#
+# This is still WIP, but working for some devices!!
+# Kernel patch may be needed for specific board quirks (sound/boards/intel)
+# (check possibility to set 16bit as default, see bytcr-rt5640.c and HP X2 detachables as an example)
+#
 for D in $(ls /usr/share/alsa/ucm); do
   if [[ $D == byt* ]] || [[ $D == cht* ]]; then
     DAC=$(aplay -l | grep $D)
@@ -7,23 +11,27 @@ for D in $(ls /usr/share/alsa/ucm); do
       echo "$DAC located"
       if [ ! -e /usr/share/alsa/ucm/$D/firsttime.done ]; then
         echo 'bytcr-init: first time run...'
-        
-	  	case "$D" in
-			bytcr-rt5651) echo 'rt5651 detected'
-			  # This device needs a initial asound.state with headphone output set and mixers enabled with
-			  # reasonable outputlevels.
-        	  cp /usr/share/alsa/ucm/$D/asound.state /var/lib/alsa
-			  /usr/sbin/alsactl restore      
-        	  echo "bytcr-init.sh: set initial output to headphones for "$(basename "$D")
-	          /usr/bin/alsaucm -c $D set _verb HiFi set _enadev Headphones
-			;;
-
-			bytcr-es8316) echo 'es8316 detected'
-			  # may have to experiment with asound.state to get reasonable levels as with the rt5651 above
-	          /usr/bin/alsaucm -c $D set _verb HiFi set _enadev Headphones
-			;;
-		esac
-
+        # Set headphones and speaker output, mixers enabled with
+	      # reasonable outputlevels (ucm defaults)
+        # Headphones are set last and thus become default
+        echo "bytcr-init.sh: set initial output to headphones for "$(basename "$D")
+  	    case "$D" in
+	        bytcr-rt5640)
+            echo "rt5640 detected"
+            /usr/bin/alsaucm -c bytcr-rt5640 set _verb HiFi set _enadev Speaker
+            /usr/bin/alsaucm -c bytcr-rt5640 set _verb HiFi set _enadev Headphones
+	        ;;
+	        bytcr-rt5651)
+            echo "rt5651 detected"
+            /usr/bin/alsaucm -c bytcr-rt5651 set _verb HiFi set _enadev Speaker
+            /usr/bin/alsaucm -c bytcr-rt5651 set _verb HiFi set _enadev Headphones
+	        ;;
+	        bytcr-es8316)
+            echo "es8316 detected"
+	          # may have to experiment with asound.state to get reasonable levels as with the rt5651 above
+	          /usr/bin/alsaucm -c ${D} set _verb HiFi set _enadev Headphones
+	        ;;
+	      esac
         echo "bytcr-init: first time run done"
         touch /usr/share/alsa/ucm/$D/firsttime.done
       fi
